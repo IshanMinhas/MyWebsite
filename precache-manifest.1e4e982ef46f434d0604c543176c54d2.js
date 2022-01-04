37fd3c745528fe10b6e6e1636014b02a322dc17f
IshanMinhas <84188309+IshanMinhas@users.noreply.github.com>
2022-01-04 12:12:47 +0530

    Updates

---
+++ precache-manifest.1e4e982ef46f434d0604c543176c54d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f3757e7c0f91201eb0c1762492f85d",
+    "url": "/Mywebsite/index.html"
+  },
+  {
+    "revision": "7819ddfea43aa9ec3221",
+    "url": "/Mywebsite/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ec825e9bd8661cdd7f8a",
+    "url": "/Mywebsite/static/js/2.48c41760.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/Mywebsite/static/js/2.48c41760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7819ddfea43aa9ec3221",
+    "url": "/Mywebsite/static/js/main.221ba773.chunk.js"
+  },
+  {
+    "revision": "88b73f7b4064852aa841",
+    "url": "/Mywebsite/static/js/runtime-main.641ffeda.js"
+  }
+]);
